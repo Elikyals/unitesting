9c247ddd052622aea45db13dc34c35c72e1ad0cf
Elikyals <kofiyalley16@gmail.com>
2022-12-14 19:59:32 +0800

    Added class to the test file

--- tests/test_function.py
+++ tests/test_function.py
@@ -2,13 +2,13 @@ import pytest
 from src.data.script import changeUppercase
 
 
-def test_for_uppercase():
-    assert changeUppercase("Eliyahu") == "ELIYAHU"
+class TestForUppercase(object):
+    def test_for_uppercase(self):
+        assert changeUppercase("Eliyahu") == "ELIYAHU"
 
-
-def test_other_for_uppercase():
-    test_argument = "Lum"
-    expected = "LUM"
-    actual = changeUppercase(test_argument)
-    message = f"changeUppercase('lum') should return {expected}, but it actually returned {actual}"
-    assert actual == expected, message
+    def test_other_for_uppercase(self):
+        test_argument = "Lum"
+        expected = "LUM"
+        actual = changeUppercase(test_argument)
+        message = f"changeUppercase('lum') should return {expected}, but it actually returned {actual}"
+        assert actual == expected, message
